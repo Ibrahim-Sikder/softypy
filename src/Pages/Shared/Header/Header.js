34eca91ffd6a254a3fe159ad9c96ce978266ecd3
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2023-05-12 02:15:57 +0600

    header-completed

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import TopBar from '../../../components/TopBar/TopBar';
-
+import './Header.css';
+import logo from '../../../Assets/logo.png';
+import { FaSistrix } from "react-icons/fa";
 const Header = () => {
     return (
         <div>
           <TopBar></TopBar>
-          <h2>THis is header</h2>  
+          <div className="header">
+            <div className='headerContent flex '>
+              <span className='logo'>
+                <img src={logo} alt="" />
+              </span>
+              <h5>About</h5>
+              <span >
+              <input placeholder='Search right hotel, visa' type="text" />
+              <FaSistrix className='searchIcon'></FaSistrix>
+              </span>
+              <button>Log in</button>
+              <button className='buttonTow'>Sign Up</button>
+            </div>
+          </div>  
         </div>
     );
 };
