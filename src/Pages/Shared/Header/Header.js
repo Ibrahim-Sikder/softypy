8647488c47b0e57b86009ae4f74968ce8c88b76d
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2023-05-12 01:32:49 +0600

    topbar-completed

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TopBar from '../../../components/TopBar/TopBar';
+
+const Header = () => {
+    return (
+        <div>
+          <TopBar></TopBar>
+          <h2>THis is header</h2>  
+        </div>
+    );
+};
+
+export default Header;
