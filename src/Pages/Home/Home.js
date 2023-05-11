53c9aaa3a575ae63af3e8f66b506c323758dd181
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2023-05-12 03:23:23 +0600

    places-completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Banner from '../../components/Banner/Banner';
+import Places from '../../components/Places/Places';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
+            <Places></Places>
             <h2>This is home page </h2>
         </div>
     );
---
+++ src/components/Places/Places.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Places.css';
+import place from '../../Assets/see2.png';
+import place2 from '../../Assets/see.png';
+import place3 from '../../Assets/travel.png';
+const Places = () => {
+    return (
+        <div className='places mt-24'>
+           <h2>Best Places To Visit In Bangladesh</h2>
+           <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+                <div>
+                    <img src={place} className='w-full' alt="" />
+                    <div className="w-full placesPrice">
+                        <span>Cox's Bazar</span>
+                        <span>1500tk</span>
+                    </div>
+                </div>
+                <div className='leftContent mb-8'>
+                    <p>Bangladesh is a South Asian country with a rich cultural heritage <br /> and natural beauty. It is home to the world's largest mangrove <br /> forest, culture, and natural beauty.</p>
+                    <div className='mt-[85px] grid grid-cols-1 md:grid-cols-2 gap-8'>
+                        <div className='w-full'>
+                            <img src={place2} className='w-full' alt="" />
+                            <div className="w-full placesPrice">
+                                <span>Cox's Bazar</span>
+                                <span>1500tk</span>
+                            </div>
+                        </div>
+                        <div className='w-full'>
+                            <img src={place3} className='w-full' alt="" />
+                            <div className="w-full placesPrice">
+                                <span>Cox's Bazar</span>
+                                <span>1500tk</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           </div>
+        </div>
+    );
+};
+
+export default Places;
